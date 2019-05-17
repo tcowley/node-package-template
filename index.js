54429db0104b18b2c75620a75cd31d16cfa505da
Trevor Cowley <tcowley@gmail.com>
2019-05-17 13:35:01 -0700

    minor comment changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// published version of package 
+// this file is what gets imported when the app is used as a package in another app.
 var template = require('./lib/index.js');
 
 module.exports = {
